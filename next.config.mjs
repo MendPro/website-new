603e9de16b761e143d9c3f1643009a77b407c88d
danieldaudu65 <danieldaudu65@gmail.com>
2025-05-28 22:17:16 +0100

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true, // if you're using server actions
+  },
+  webpack: (config, { isServer }) => {
+    // Fix styled-components or fs module issues on client side
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+
+    return config
+  },
+}
 
 export default nextConfig
